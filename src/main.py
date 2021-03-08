8cc7e038764655e90c6b14dd5c85eac6bd25d494
Float07 <thiago.d.c.de.pauli@gmail.com>
2021-03-08 16:44:10 -0300

    Removed some prints

--- src/main.py
+++ src/main.py
@@ -29,7 +29,6 @@ def start_flask_app():
     @app.route("/")
     def respond():
         return("Oi tudo bom? :3")
-    print("/" + TOKEN)
     @app.route("/" + TOKEN,methods=["POST", "GET"])
     def respond_API():
         update_json = request.json
@@ -37,7 +36,6 @@ def start_flask_app():
             fn.process_message(update_json["message"])
             return "ok"
         return "Ué vc n é a API do Telegram. SAI DAQUI VEEEEEEI"
-    print("Iniciando apliacação")
     app.run(host='0.0.0.0', port=PORT)
 
 
