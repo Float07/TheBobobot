c3e999eb527facb1f41051432f10a78cf42c8f4f
Float07 <39651883+Float07@users.noreply.github.com>
2022-06-19 21:15:15 -0300

    get database url from env

--- src/database_handler.py
+++ src/database_handler.py
@@ -1,9 +1,15 @@
 import psycopg2
+import os
 
+DATABASE_URL = ""
+try:
+    DATABASE_URL = os.environ["DATABASE_URL"]
+except KeyError:
+    print("Sem variáveis de ambiente DATABASE_URL!")
 
 def get_registered_user(user_id):
     try:
-        conn = psycopg2.connect("postgres://fkvxayzijvnjgu:451a996eb499da4342fbf3504caf3dfe9e52a67f3bbf0474b30cb899bc2dd172@ec2-3-208-224-152.compute-1.amazonaws.com:5432/dcj1dv4k3ghlig")
+        conn = psycopg2.connect(DATABASE_URL)
     except:
         print("Error connecting to data bank!")
     query = f"""SELECT group_id FROM registered_anon 
@@ -19,7 +25,7 @@ def get_registered_user(user_id):
 
 def set_user_group(user_id, group_id):
     try:
-        conn = psycopg2.connect("postgres://fkvxayzijvnjgu:451a996eb499da4342fbf3504caf3dfe9e52a67f3bbf0474b30cb899bc2dd172@ec2-3-208-224-152.compute-1.amazonaws.com:5432/dcj1dv4k3ghlig")
+        conn = psycopg2.connect(DATABASE_URL)
     except:
         print("Error connecting to data bank!")
     query = f"""INSERT INTO registered_anon(user_id, group_id)
